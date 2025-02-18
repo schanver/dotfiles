ad3225854740bd896e0ec1612ce97f454c72530a
schanver <mehmet-fatih.sanver@tu-dortmund.de>
2025-02-18 18:43:49 +0100

    some config

--- doom/custom.el
+++ doom/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("/home/f/org/roam/todolist.org" "/home/f/org/roam/uni/prml.org" "/home/f/org/roam/uni/get_1.org" "/home/f/org/roam/uni/swk.org" "/home/f/org/roam/uni/rvs.org"))
+   '("~/org/roam/uni/signale_und_systeme.org" "/home/f/org/roam/uni/bs.org" "/home/f/org/roam/uni/mafi2.org" "/home/f/org/roam/uni/ubersetzerbau.org" "/home/f/org/roam/uni/get_2.org" "/home/f/org/roam/todolist.org" "/home/f/org/roam/uni/prml.org" "/home/f/org/roam/uni/get_1.org" "/home/f/org/roam/uni/swk.org" "/home/f/org/roam/uni/rvs.org"))
  '(package-selected-packages
    '(org-fragtog org-roam-ui websocket org-alert org-roam emacsql)))
 (custom-set-faces
