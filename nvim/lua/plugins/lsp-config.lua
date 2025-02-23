0f48e647d9436d3a380760e58a15ea38f6db6967
schanver <mehmet-fatih.sanver@tu-dortmund.de>
2025-02-23 23:50:14 +0100

    added new plugin and new lsp for c

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -38,6 +38,9 @@ return {
       lspconfig.jedi_language_server.setup({
         capabilities = capabilities
       })
+      lspconfig.clangd.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
