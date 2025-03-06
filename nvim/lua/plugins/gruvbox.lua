933f89002635ed383b4006962dc2b15a7aa82758
schanver <mehmet-fatih.sanver@tu-dortmund.de>
2025-03-06 14:12:50 +0100

    thursday commit

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -1,7 +1,8 @@
-return 
-  { 
+return
+  {
     "ellisonleao/gruvbox.nvim",
-    priority = 1000 , 
-    config = true, 
-    opts = ...
+    priority = 1000 ,
+    config = function()
+      vim.cmd.colorscheme "gruvbox"
+    end,
   }
