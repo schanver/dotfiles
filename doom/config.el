221bf2bd64d35247b467fb46e7e094023d35eda8
schanver <mehmet-fatih.sanver@tu-dortmund.de>
2025-03-06 22:51:18 +0100

    changed font

--- doom/config.el
+++ doom/config.el
@@ -37,6 +37,7 @@
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
+(setq doom-font (font-spec :family "IosevkaNFM" :size 22))
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
