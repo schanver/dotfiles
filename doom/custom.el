f0ca54ffcec1cc4b0de331b9f7f744beffe66861
schanver <mehmet-fatih.sanver@tu-dortmund.de>
2025-05-09 16:55:19 +0200

    friday commit

--- doom/custom.el
+++ doom/custom.el
@@ -4,10 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/org/roam/todolist.org" "/home/f/org/roam/uni/tim.org"
-     "/home/f/org/roam/uni/etkt.org" "/home/f/org/roam/uni/ubersetzerbau.org"
-     "/home/f/org/roam/uni/bs.org" "/home/f/org/roam/uni/fupro.org"
-     "/home/f/org/roam/uni/mafi2.org" "/home/f/org/roam/uni/ss25.org"))
+   '("~/org/roam/uni/ubersetzerbau.org" "/home/mfs/org/roam/uni/fupro.org"
+     "/home/mfs/org/roam/uni/tim.org" "/home/mfs/org/roam/todolist.org"
+     "/home/mfs/org/roam/uni/bs.org" "/home/mfs/org/roam/uni/mafi2.org"
+     "/home/mfs/org/roam/uni/ss25.org"))
  '(package-selected-packages
    '(all-the-icons focus olivetti org-alert org-fragtog org-habit-stats org-modern
      org-roam-ui pdf-tools)))
