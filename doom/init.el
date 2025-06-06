b9090ebf511323ea536e85c021057be0bbdd3f88
schanver <mehmet-fatih.sanver@tu-dortmund.de>
2025-06-06 02:06:15 +0200

    :*

--- doom/init.el
+++ doom/init.el
@@ -154,7 +154,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +habit +dragndrop +pretty)  ; organize your plain life in plain text
+       (org +babel +habit +dragndrop +pretty)  ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
@@ -191,8 +191,5 @@
        ;;(rss +org)        ; emacs as an RSS reader
 
        :config
-       ;;literate
+       literate
        (default +bindings +smartparens))
-(setq
- org-superstar-headline-bullets-list '("⁖" "◉" "○" "✸" "✿")
- )
