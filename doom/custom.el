3c5548af4f25f49f5b9f8715348530ac2fff98aa
schanver <mehmet-fatih.sanver@tu-dortmund.de>
2025-10-10 12:06:20 +0200

    friday commit

--- doom/custom.el
+++ doom/custom.el
@@ -3,7 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("/home/mfs/org/gcal.org")))
+ '(org-agenda-files
+   '("/home/mfs/org/roam/daily/2025-09-15.org" "/home/mfs/org/roam/uni/tim.org"
+     "/home/mfs/org/roam/uni/ws25_26.org" "/home/mfs/org/roam/media_tracker.org"
+     "/home/mfs/org/gcal.org"))
+ '(package-selected-packages
+   '(all-the-icons calfw-gcal focus hl-column multicolumn olivetti org-alert
+     org-contrib org-fragtog org-gcal org-habit-stats org-modern org-movies
+     org-notify org-roam-ui pdf-tools)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
