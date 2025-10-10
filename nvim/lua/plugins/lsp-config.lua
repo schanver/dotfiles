760d9a6cd6a2bfb16e1198cd79e2688352c6ed2c
schanver <mehmet-fatih.sanver@tu-dortmund.de>
2025-10-10 12:09:48 +0200

    sync

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -17,55 +17,31 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.tailwindcss.setup({ capabilities = capabilities })
-      lspconfig.ts_ls.setup({ capabilities = capabilities })
-      lspconfig.solargraph.setup({ capabilities = capabilities })
-      lspconfig.html.setup({ capabilities = capabilities })
-      lspconfig.lua_ls.setup({ capabilities = capabilities })
-      lspconfig.jedi_language_server.setup({ capabilities = capabilities })
-      lspconfig.clangd.setup({ capabilities = capabilities })
-      lspconfig.jdtls.setup({ capabilities = capabilities })
-      lspconfig.kotlin_language_server.setup({ capabilities = capabilities })
-
-      -- Diagnostic signs
-      local signs = {
-        { name = "DiagnosticSignError", text = "" },
-        { name = "DiagnosticSignWarn",  text = "" },
-        { name = "DiagnosticSignInfo",  text = "" },
-        { name = "DiagnosticSignHint",  text = "" },
+      local servers = {
+        "tailwindcss",
+        "ts_ls",
+        "html",
+        "lua_ls",
+        "jedi_language_server",
+        "clangd",
+        "jdtls",
+        "kotlin_language_server",
+        "bashls",
       }
-      for _, sign in ipairs(signs) do
-        vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-      end
 
-      -- Diagnostic config
-      vim.diagnostic.config({
-        virtual_text = { prefix = "●", spacing = 2 },
-        signs = true,
-        underline = true,
-        update_in_insert = false,
-        severity_sort = true,
-        float = {
-          border = "rounded",
-          source = "always",
-          focusable = false,
-        },
+      vim.lsp.config("*", {
+        capabilities = capabilities,
+        root_markers = { ".git" },
       })
 
-      -- Show diagnostics popup on CursorHold
-      vim.cmd([[
-        autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
-        ]])
-
+      -- keymaps
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
-      vim.keymap.set('n','<leader>e', vim.diagnostic.open_float, { noremap = true, silent = true })
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
     end,
   },
 }
