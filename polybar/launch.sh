9ff02f29d4df10e1b0bdcfa69abd86aa198634c4
schanver <mehmet-fatih.sanver@tu-dortmund.de>
2025-12-29 00:00:07 +0100

    delete i3status and polybar

--- polybar/launch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-killall polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-polybar main -c ~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
